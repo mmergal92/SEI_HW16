69c0efa05a0d37bf25863489eaa5e8523ff61707
mmergal92 <mmergal92@gmail.com>
2021-05-19 22:41:29 -0400

    adding movie files copied

---
+++ movie-search/src/App.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import './App.css';
+//IMPORT COMPONENTS
+import MovieDisplay from './components/MovieDisplay';
+import Form from './components/Form';
+
+function App() {
+//API KEY
+  const apiKey="1263f44d";
+//STATE
+  const [movie, setMovie] = useState(null);
+//FUNCTION to getMovies
+  const getMovie = async(searchTerm)=>{
+    //fetch request and store response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    )
+    //parse JSON into object
+    const data = await response.json();
+    //set Movie state to movie
+    setMovie(data)
+    console.log(data)
+  }
+  //New array for Data
+  // const dataArray = async(random)=>{
+  //   const response = await fetch(
+  //     `http://www.omdbapi.com/?apikey=${apiKey}&t=${random}`
+  //   )
+  // }
+  //Use Effect
+  React.useEffect(()=>{getMovie("Clueless")},[])
+  // React.useEffect(()=>{(dataArray)},[])
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie}/>
+      <MovieDisplay movie ={movie}/>
+    </div>
+  );
+}
+
+export default App;
